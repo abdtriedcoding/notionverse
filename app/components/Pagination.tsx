d4d7ced4b3df5d6c5c5f64f55242f7e53ec0007e
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-12-05 15:15:33 +0530

    ⭐ style the pagination button

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,19 +1,27 @@
+import { Button } from "@/components/ui/button";
 import { PaginationProps } from "@/types";
 import Link from "next/link";
-import React from "react";
 
 const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages }) => {
   return (
-    <div className="max-w-2xl m-auto px-4 items-center flex flex-row justify-between">
+    <div
+      className={`max-w-2xl m-auto px-4 items-center flex flex-row ${
+        currentPage > 1 ? "justify-between" : "justify-end"
+      }`}
+    >
       {currentPage > 1 && (
-        <Link href={`/?page=${currentPage - 1}`} passHref>
-          Previous
+        <Link href={`/?page=${currentPage - 1}`}>
+          <Button>
+            <span>&larr; Previous</span>
+          </Button>
         </Link>
       )}
 
       {currentPage < totalPages && (
-        <Link href={`/?page=${currentPage + 1}`} passHref>
-          Next
+        <Link href={`/?page=${currentPage + 1}`}>
+          <Button>
+            <span>Next &rarr;</span>
+          </Button>
         </Link>
       )}
     </div>
