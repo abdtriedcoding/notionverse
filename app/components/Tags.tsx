c8cb3710105e60cc68658059e7a5f4b5be5d7111
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-12-06 02:27:54 +0530

    refactor the tags component

--- app/components/Tags.tsx
+++ app/components/Tags.tsx
@@ -1,17 +1,25 @@
 import { Button } from "@/components/ui/button";
 import { TagFrequencyMap } from "@/types";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 const Tags = ({ tagFrequencyMap }: { tagFrequencyMap: TagFrequencyMap }) => {
+  const params = useParams();
+  const { slug } = params;
+
   return (
     <div className="max-w-2xl space-x-3 overflow-x-auto whitespace-nowrap">
-      {Object.entries(tagFrequencyMap).map(([tag, value], index) => (
-        <Link href={`/tag/${tag}`}>
-          <Button key={index}>
-            {tag} <span className="ml-2">({value})</span>
-          </Button>
-        </Link>
-      ))}
+      {Object.entries(tagFrequencyMap).map(([tag, value], index) => {
+        const selected = tag === slug;
+
+        return (
+          <Link href={selected ? "/search" : `/tag/${tag}`} key={index}>
+            <Button className={selected ? "bg-gray-500" : ""}>
+              {tag} <span className="ml-2">({value})</span>
+            </Button>
+          </Link>
+        );
+      })}
     </div>
   );
 };
